685194b1c6586d7f399dfcdfc1cbb61de4a32021
Aditya Dhingra <adityadhingra07@gmail.com>
2015-10-30 20:33:00 -0400

    SessionCookie Class
    .

---
+++ src/SessionCookie.java
@@ -0,0 +1,25 @@
+/**
+ * Created by Aditya on 10/30/15.
+ */
+public class SessionCookie {
+
+    private int cookieID;
+    public static int timeoutLength;
+
+    public SessionCookie(long id) {
+        
+    }
+
+    public boolean hasTimedOut() {
+
+    }
+
+    public void updateTimeOfActivity() {
+
+    }
+
+    public long getID() {
+
+    }
+
+}
--- src/User.java
+++ src/User.java
@@ -1,6 +1,22 @@
 
 public class User {
-	
-	// TODO complete this class
+
+    //PROTOCOLS FOR USER.CLASS
+
+	public void ADD_USER(int cookieID, String username, String password) {
+
+    }
+
+    public void USER_LOGIN(String username, String password) {
+
+    }
+
+    public void POST_MESSAGE(int cookieID, String message) {
+
+    }
+
+    public void GET_MESSAGES(int CookieID, int numMessages) {
+
+    }
 
 }
