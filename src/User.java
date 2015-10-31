e9786624c215bbfa3a85c0b5732b48211ade3af6
MysticOne17 <AbidsMac@pal-nat184-077-246.itap.purdue.edu>
2015-10-30 21:16:37 -0400

    User class update 1
    user class updated

--- src/User.java
+++ src/User.java
@@ -1,14 +1,48 @@
+import java.util.ArrayList;
 
 public class User {
 
     //PROTOCOLS FOR USER.CLASS
 
-	public void ADD_USER(int cookieID, String username, String password) {
+    private String username;
+    private String password;
+    private SessionCookie cookie;
+    ArrayList<User> userArrayList;
 
+
+    public User(String username, String password, SessionCookie cookie) {
+        this.username = username;
+        this.password = password;
+        this.cookie = cookie;
+        userArrayList = new ArrayList<User>;
+    }
+
+    public String getName() {
+        return username;
+    }
+
+    public boolean checkPassword(String password) {
+        if (this.password.equals(password)) {
+            return true;
+        }
+        return false;
+    }
+
+    public SessionCookie getCookie() {
+        return cookie;
+    }
+
+    public void setCookie(SessionCookie cookie) {
+        this.cookie = cookie;
+    }
+    public void ADD_USER(int cookieID, String username, String password) {
+        //  User userNew = new User(password, username, cookieID);
     }
 
     public void USER_LOGIN(String username, String password) {
-
+        for (int i = 0; i < userArrayList.size(); i++) {
+            
+        }
     }
 
     public void POST_MESSAGE(int cookieID, String message) {
