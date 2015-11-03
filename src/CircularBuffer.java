c8ff207ccc9ecc5dd626d3527c845cf1dfa2fffc
MysticOne17 <AbidsMac@pal-nat184-078-111.itap.purdue.edu>
2015-11-03 12:21:37 -0500

    Circular Buffer update
    Almost done, just one method left

--- src/CircularBuffer.java
+++ src/CircularBuffer.java
@@ -1,5 +1,23 @@
 /**
  * Created by Aditya on 10/30/15.
  */
+import java.util.*;
 public class CircularBuffer {
+    private int size;
+    public CircularBuffer(int size) {
+        this.size = size;
+    }
+
+    public void put(String message) {
+        Random randomifier = new Random();
+
+        int num1 = randomifier.nextInt(9);
+        int num2 = randomifier.nextInt(9);
+        int num3 = randomifier.nextInt(9);
+        int num4 = randomifier.nextInt(9);
+        String FourCode = "" + num1 + num2 + num3 + num4;
+        message = FourCode + ") " + message;
+    }
+
+
 }
