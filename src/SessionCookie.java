69a7cafa5ee9d60bde734d33266329fd8b3e3f4c
Aditya Dhingra <adityadhingra07@gmail.com>
2015-11-14 01:15:40 -0500

    Test Cases Added
    test cases

--- src/SessionCookie.java
+++ src/SessionCookie.java
@@ -1,6 +1,15 @@
 /**
- * Created by Aditya Dhingra & Abid Kaisani on 10/30/15.
+ * <b> CS 180 - Project 4 - Chat Server Skeleton </b>
+ * <p>
+ * <p>
+ * This is the skeleton code for the ChatServer Class. This is a private chat
+ * server for you and your friends to communicate.
+ *
+ * @author (Aditya Dhingra & Abid Kaisani) <(adhingr@purdue.edu ,akaisani@purdue.edu)>
+ * @version (Today's Date)
+ * @lab (Your Lab Section)
  */
+
 import java.util.*;
 public class SessionCookie {
 
--- src/User.java
+++ src/User.java
@@ -1,6 +1,15 @@
 /**
- * Aditya Dhingra & Abid Kaisani
+ * <b> CS 180 - Project 4 - Chat Server Skeleton </b>
+ * <p>
+ * <p>
+ * This is the skeleton code for the ChatServer Class. This is a private chat
+ * server for you and your friends to communicate.
+ *
+ * @author (Aditya Dhingra & Abid Kaisani) <(adhingr@purdue.edu ,akaisani@purdue.edu)>
+ * @version (Today's Date)
+ * @lab (Your Lab Section)
  */
+
 import java.util.ArrayList;
 
 public class User {
